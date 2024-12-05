080353d408352a9945b3185869966742631a7f08
Jon Dafoe <juanzafran121@gmail.com>
2024-12-05 05:57:28 -0300

    mostrar-actividad se traen las actividades. mejor estilo. nuevo componente boton-atras por ahora solo implementado en mostrar-actividades

--- src/app/actividades/mostrar-actividad/mostrar-actividad.component.ts
+++ src/app/actividades/mostrar-actividad/mostrar-actividad.component.ts
@@ -1,30 +1,34 @@
 import { Component, OnInit } from '@angular/core';
 import { ActividadService } from '../actividad.service.js';
-import { idText } from 'typescript';
 import { RouterLink } from '@angular/router';
+import { BotonAtrasComponent } from '../../boton-atras/boton-atras.component.js';
 
 @Component({
   selector: 'app-mostrar-actividad',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink,BotonAtrasComponent],
   templateUrl: './mostrar-actividad.component.html',
   styleUrl: './mostrar-actividad.component.css'
 })
 export class MostrarActividadComponent implements OnInit{
   constructor (public service : ActividadService){}
   bandera:undefined|boolean
+  actividades:{cantidad_minima:number, cod_actividad:number, cod_sector:number, descripcion:string,
+dia_de_la_semana:number, edad_minima:number,  estado:number,  hora_fin:number, hora_inicio:number,
+ locacion:string,nombre:string}[]|undefined
+  estadoActividad: string|undefined
   ngOnInit(): void {
     this.service.getActividades().subscribe({
       next:(data)=>{
-        if(data.status === undefined){
-          this.service.actividades=data
-          console.log("actividades encontradas" , data)
+        if(data.status === 201){
+          this.actividades = data.data
+          console.log("Actividades Encontradas:" , this.actividades)
         }
       },
       error:(e)=>{
         if(e.status === 404){
           this.bandera = true
-          console.log("actividades no encontradas ",e)
+          console.log("Actividades NO Encontradas:",e)
         }
       }})
   }
